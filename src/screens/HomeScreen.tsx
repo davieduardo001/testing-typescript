a50f87b72f5631de1716903b91143e895b51ae69
davieduardocosta001 <davieduardocosta01@gmail.com>
2022-09-11 09:14:44 -0300

    set variables

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import { View, StyleSheet, Text } from "react-native"
 
+
 export default function App() {
-  return(
-    <View style={styles.container}>
-       <Text style={styles.text}>TEST</Text> 
-    </View>
-  )
+
+    interface User {
+        name: String,
+        baseEnthusiasmLevel: Number,
+    }
+
+    const user: User = {
+        name: 'Jorg',
+        baseEnthusiasmLevel: 0
+    }
+
+    const [enthusiasmLevel, setEnthusiasmLevel] = useState(0)
+
+    return(
+        <View style={styles.container}>
+           <Text style={styles.text}>{user.name}</Text> 
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
